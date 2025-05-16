835e121ac2e80ca53e989ff35a6ab70bc49b06cd
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-16 10:07:06 +0100

    Add library outline
    Add build tools.
    Add library outline.
    Add tests outline.

---
+++ src/amulet/game/temp.cpp
@@ -0,0 +1,9 @@
+#include <amulet/game/dll.hpp>
+
+namespace Amulet {
+
+AMULET_GAME_EXPORT void _temp()
+{
+}
+
+}
---
+++ tests/test_amulet_game/test_abc.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+class JavaGameTestCase(TestCase):
+    def test_temp(self) -> None:
+        pass
