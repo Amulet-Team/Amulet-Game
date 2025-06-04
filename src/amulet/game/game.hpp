444c3b483d618818dd1bc5d68e2457b66b058599
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-04 16:43:03 +0100

    Add missing export symbols

--- src/amulet/game/game.hpp
+++ src/amulet/game/game.hpp
@@ -3,15 +3,18 @@
 #include <memory>
 #include <string>
 
-#include "abc/version.hpp"
-#include "java/version.hpp"
 #include <amulet/core/version/version.hpp>
+
+#include <amulet/game/abc/version.hpp>
+#include <amulet/game/java/version.hpp>
+
+#include <amulet/game/dll.hpp>
 
 namespace Amulet {
 namespace game {
 
-    std::shared_ptr<GameVersion> get_game_version(const std::string&, const VersionNumber& version);
-    std::shared_ptr<JavaGameVersion> get_java_game_version(const VersionNumber& version);
+    AMULET_GAME_EXPORT std::shared_ptr<GameVersion> get_game_version(const std::string&, const VersionNumber& version);
+    AMULET_GAME_EXPORT std::shared_ptr<JavaGameVersion> get_java_game_version(const VersionNumber& version);
 
 } // namespace game
 } // namespace Amulet
