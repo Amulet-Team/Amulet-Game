391fbe3ce84d985ae70959aae074104c8ee8e713
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-26 08:22:46 +0100

    Move build dir to temp

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import glob
 import json
 import gzip
 import pickle
+from tempfile import TemporaryDirectory
 
 from setuptools import setup, Extension, Command
 from setuptools.command.build import build
@@ -65,32 +66,33 @@ class CMakeBuild(cmdclass.get("build_ext", build_ext)):
 
         if subprocess.run(["cmake", "--version"]).returncode:
             raise RuntimeError("Could not find cmake")
-        if subprocess.run(
-            [
-                "cmake",
-                *platform_args,
-                f"-DPYTHON_EXECUTABLE={sys.executable}",
-                f"-Dpybind11_DIR={fix_path(pybind11.get_cmake_dir())}",
-                f"-Damulet_pybind11_extensions_DIR={fix_path(amulet.pybind11_extensions.__path__[0])}",
-                f"-Damulet_io_DIR={fix_path(amulet.io.__path__[0])}",
-                f"-Damulet_nbt_DIR={fix_path(amulet.nbt.__path__[0])}",
-                f"-Damulet_core_DIR={fix_path(amulet.core.__path__[0])}",
-                f"-Damulet_game_DIR={fix_path(game_src_dir)}",
-                f"-DAMULET_GAME_EXT_DIR={fix_path(ext_dir)}",
-                f"-DCMAKE_INSTALL_PREFIX=install",
-                "-B",
-                "build",
-            ]
-        ).returncode:
-            raise RuntimeError("Error configuring amulet-game")
-        if subprocess.run(
-            ["cmake", "--build", "build", "--config", "Release"]
-        ).returncode:
-            raise RuntimeError("Error installing amulet-game")
-        if subprocess.run(
-            ["cmake", "--install", "build", "--config", "Release"]
-        ).returncode:
-            raise RuntimeError("Error installing amulet-game")
+        with TemporaryDirectory() as tempdir:
+            if subprocess.run(
+                [
+                    "cmake",
+                    *platform_args,
+                    f"-DPYTHON_EXECUTABLE={sys.executable}",
+                    f"-Dpybind11_DIR={fix_path(pybind11.get_cmake_dir())}",
+                    f"-Damulet_pybind11_extensions_DIR={fix_path(amulet.pybind11_extensions.__path__[0])}",
+                    f"-Damulet_io_DIR={fix_path(amulet.io.__path__[0])}",
+                    f"-Damulet_nbt_DIR={fix_path(amulet.nbt.__path__[0])}",
+                    f"-Damulet_core_DIR={fix_path(amulet.core.__path__[0])}",
+                    f"-Damulet_game_DIR={fix_path(game_src_dir)}",
+                    f"-DAMULET_GAME_EXT_DIR={fix_path(ext_dir)}",
+                    f"-DCMAKE_INSTALL_PREFIX=install",
+                    "-B",
+                    tempdir,
+                ]
+            ).returncode:
+                raise RuntimeError("Error configuring amulet-game")
+            if subprocess.run(
+                ["cmake", "--build", tempdir, "--config", "Release"]
+            ).returncode:
+                raise RuntimeError("Error installing amulet-game")
+            if subprocess.run(
+                ["cmake", "--install", tempdir, "--config", "Release"]
+            ).returncode:
+                raise RuntimeError("Error installing amulet-game")
 
 
 class MinifyJSON(Command):
