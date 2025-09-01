7b5a3ac3255071e960841240c349f8943e5da274
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-09-01 13:30:40 +0100

    Improve build tools

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import subprocess
 import sys
 from pathlib import Path
 import platform
-import datetime
 import glob
 import json
 import gzip
@@ -14,18 +13,9 @@ from setuptools import setup, Extension, Command
 from setuptools.command.build import build
 from setuptools.command.build_ext import build_ext
 
-from packaging.version import Version
-
 import versioneer
 
 import requirements
-
-if (
-    os.environ.get("AMULET_FREEZE_COMPILER", None)
-    and sys.platform == "darwin"
-    and platform.machine() != "arm64"
-):
-    raise Exception("The MacOS frozen build must be created on arm64")
 
 
 def fix_path(path: str) -> str:
@@ -40,6 +30,7 @@ class CMakeBuild(cmdclass.get("build_ext", build_ext)):
         import pybind11
         import amulet.pybind11_extensions
         import amulet.io
+        import amulet.utils
         import amulet.nbt
         import amulet.core
 
@@ -75,6 +66,7 @@ class CMakeBuild(cmdclass.get("build_ext", build_ext)):
                     f"-Dpybind11_DIR={fix_path(pybind11.get_cmake_dir())}",
                     f"-Damulet_pybind11_extensions_DIR={fix_path(amulet.pybind11_extensions.__path__[0])}",
                     f"-Damulet_io_DIR={fix_path(amulet.io.__path__[0])}",
+                    f"-Damulet_utils_DIR={fix_path(amulet.utils.__path__[0])}",
                     f"-Damulet_nbt_DIR={fix_path(amulet.nbt.__path__[0])}",
                     f"-Damulet_core_DIR={fix_path(amulet.core.__path__[0])}",
                     f"-Damulet_game_DIR={fix_path(game_src_dir)}",
@@ -161,35 +153,8 @@ cmdclass.get("build", build).sub_commands.append(("minify_json", None))
 cmdclass["build_ext"] = CMakeBuild
 
 
-def _get_version() -> str:
-    version_str: str = versioneer.get_version()
-
-    if os.environ.get("AMULET_FREEZE_COMPILER", None):
-        date_format = "%y%m%d%H%M%S"
-        try:
-            with open("build/timestamp.txt", "r") as f:
-                timestamp = datetime.datetime.strptime(f.read(), date_format)
-        except Exception:
-            timestamp = datetime.datetime(1, 1, 1)
-        if datetime.timedelta(minutes=10) < datetime.datetime.now() - timestamp:
-            timestamp = datetime.datetime.now()
-            os.makedirs("build", exist_ok=True)
-            with open("build/timestamp.txt", "w") as f:
-                f.write(timestamp.strftime(date_format))
-
-        version = Version(version_str)
-        epoch = f"{version.epoch}!" if version.epoch else ""
-        release = ".".join(map(str, version.release))
-        pre = "".join(map(str, version.pre)) if version.is_prerelease else ""
-        post = f".post{timestamp.strftime(date_format)}"
-        local = f"+{version.local}" if version.local else ""
-        version_str = f"{epoch}{release}{pre}{post}{local}"
-
-    return version_str
-
-
 setup(
-    version=_get_version(),
+    version=versioneer.get_version(),
     cmdclass=cmdclass,
     ext_modules=[Extension("amulet.game._amulet_game", [])]
     * (not os.environ.get("AMULET_SKIP_COMPILE", None)),
