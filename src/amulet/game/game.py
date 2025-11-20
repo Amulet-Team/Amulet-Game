a04aa7e0f5db119a618453ba42b2ceffb2367ade
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-11-20 09:41:50 +0000

    Reformat

--- src/amulet/game/game.py
+++ src/amulet/game/game.py
@@ -30,6 +30,7 @@ def _get_versions() -> dict[str, list[GameVersion]]:
         if _versions is None:
             _log.debug("Loading Minecraft translations")
             from .abc import GameVersion
+
             pkl_path = os.path.join(os.path.dirname(__file__), "versions.pkl.gz")
             with open(pkl_path, "rb") as pkl:
                 versions: object = pickle.loads(gzip.decompress(pkl.read()))
