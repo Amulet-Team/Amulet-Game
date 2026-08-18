1b6c7fca52b1583af98d95dd9cbd57f44365f762
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-08-18 09:08:59 +0100

    Move root module variables into C++

--- src/amulet/game/_amulet_game_.py.cpp
+++ src/amulet/game/_amulet_game_.py.cpp
@@ -2,8 +2,20 @@
 
 namespace py = pybind11;
 
-void init_java(py::module);
+py::module init_java(py::module);
 
-void init_amulet_game(py::module m){
-    init_java(m);
+void init_amulet_game(py::module m)
+{
+    m.attr("__doc__") = "A module to store data about the game including state enumerations and translations between different game versions.";
+
+    auto game_module = py::module::import("amulet.game.game");
+    m.attr("get_game_platforms") = game_module.attr("get_game_platforms");
+    m.attr("get_game_versions") = game_module.attr("get_game_versions");
+    m.attr("get_game_version") = game_module.attr("get_game_version");
+
+    auto java_module = init_java(m);
+    m.attr("JavaGameVersion") = java_module.attr("JavaGameVersion");
+
+    auto bedrock_module = py::module::import("amulet.game.bedrock");
+    m.attr("BedrockGameVersion") = bedrock_module.attr("BedrockGameVersion");
 }
